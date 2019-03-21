028e82811ada0eec34b3952c0692686d96a9380e
Laura Rocha Prado <laurarochaprado@gmail.com>
2019-03-21 11:47:39 -0500

    added bash script to generate jekyll posts from command line in local repo

---
+++ _posts/newpost.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# define parameters which are passed in.
+DATE=$1 # YYYY-MM-DD
+TITLE=$2 # Title or "Long title"
+CATS=$3 # category OR "category 1, category 2"
+TEXT=$4 # "Full text of post"
+FILENAME=$1-$5 # short-title-hifenated
+
+NewFile=$FILENAME.md
+(
+# define the template.
+cat  << EOF
+---
+layout: post
+title: "$TITLE"
+categories: $CATS
+author: "Laura Rocha Prado"
+---
+
+$TEXT
+
+EOF
+) > $NewFile
+
+# usage
+# LINUX: newpost.sh "This is a post" category "This is the content of the post" a-short-title
+# MAC: sh newpost.sh "This is a post" category "This is the content of the post" a-short-title
