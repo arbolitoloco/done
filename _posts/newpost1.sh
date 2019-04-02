7048ab1a4a496b890aa99c09f137dea36b80a031
Laura Rocha Prado <laurarochaprado@gmail.com>
2019-04-02 14:17:44 -0500

    improved output for interactive script and instructions

--- _posts/newpost1.sh
+++ _posts/newpost1.sh
@@ -2,20 +2,32 @@
 
 # prompt user for parameters
 read -e -p "Enter the date (YYYY-MM-DD): " DATE
+echo "**************"
 echo $DATE
+echo "**************"
 
 read -e -p "Enter the post long title: " TITLE
+echo "**************"
 echo $TITLE
+echo "**************"
 
 read -e -p "Enter the post short title: " SHORT 
 SHORT="$(tr ' ' '-' <<<$SHORT)" # replaces spaces for hiphens
+echo "**************"
 echo $SHORT
+echo "**************"
 
+CATFILES=$(head -n 1 categories.txt)
+echo "From categories:" $CATFILES
 read -e -p "Enter the category: " CATS
+echo "**************"
 echo $CATS
+echo "**************"
 
 read -e -p "Enter the full text of post: " TEXT
+echo "**************"
 echo $TEXT
+echo "**************"
 
 NewFile=$DATE-$SHORT.md # output is DATE-FILENAME.md
 (
@@ -33,6 +45,13 @@ $TEXT
 EOF
 ) > $NewFile
 
+echo "**************"
+echo "Post has been created! Don't forget to push to origin."
+echo "**************"
+echo "Bye now."
+
 # usage
-# LINUX: newpost.sh "This is a post" category "This is the content of the post" a-short-title
-# MAC: sh newpost.sh "This is a post" category "This is the content of the post" a-short-title
+# LINUX: newpost1.sh and follow instructions
+# MAC: sh newpost1.sh and follow instructions
+# WINDOWS: open Git Bash, cd to C:/path/to/repo/_posts/ 
+# then run sh newpost1.sh and follow instructions
